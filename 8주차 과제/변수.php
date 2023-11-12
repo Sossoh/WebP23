0c054d791c82c1baa7cc35309129512a5c720cb6
Sossoh <128332587+Sossoh@users.noreply.github.com>
2023-11-12 22:18:19 +0900

    Update 변수.php

--- 8주차 과제/변수.php
+++ 8주차 과제/변수.php
@@ -1,27 +1,28 @@
 <?php
 
-$userInput = $_POST['user_input']; 
+if (isset($_POST['user_input'])) {
+    $userInput = $_POST['user_input'];
 
-if (is_numeric($userInput)) {
-    $result = processNumber($userInput);
+    if (is_numeric($userInput)) {
+        $result = processNumber((int)$userInput); 
 
-
-    echo "입력 값: $userInput, 출력 값: $result";
+        echo "입력 값: " . htmlspecialchars($userInput) . ", 출력 값: " . htmlspecialchars($result);
+    } else {
+        echo "숫자를 입력하세요.";
+    }
 } else {
-    echo "숫자를 입력하세요.";
+    echo "입력 값을 찾을 수 없습니다.";
 }
-
 
 function processNumber($a) {
     if ($a % 2 == 1) {
-        $result = $a + 1; 
+        $result = $a + 1;
     } else {
         $result = $a;
     }
 
     return $result;
 }
-
 ?>
 
 
