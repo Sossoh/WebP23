ad401e5e4c8589aec08b322355a1e1306435d9ed
sohee <dfroug@naver.com>
2023-11-18 20:56:44 +0900

    #ver1commit

--- 9주차 과제/고객읽기.php
+++ 9주차 과제/고객읽기.php
@@ -9,17 +9,14 @@
 <?php
 $filename = 'client.txt';
 
-// 파일이 존재하는지 확인
 if (file_exists($filename)) {
     $lines = file($filename, FILE_IGNORE_NEW_LINES);
 
-
     foreach ($lines as $line) {
-        // 탭으로 구분된 값을 배열로 분할
         $data = explode("\t", $line);
         
         if (isset($data[1]) && intval($data[1]) >= 30) {
-            echo "이름: {$data[0]}, 나이: {$data[1]}, 성별: {$data[2]}, 이메일: {$data[3]}\n";
+            echo "이름: {$data[0]}, 나이: {$data[1]}, 성별: {$data[2]}, 이메일: {$data[3]}<br>";
         }
     }
 } else {
