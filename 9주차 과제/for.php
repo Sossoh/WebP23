f9b1c8d76c4d42893b4cc95eb90f39d856fdfb29
sohee <dfroug@naver.com>
2023-11-18 20:46:36 +0900

    #ver1commit

--- 9주차 과제/for.php
+++ 9주차 과제/for.php
@@ -8,20 +8,19 @@
 <?php
 $rows = 5;
 
-
 for ($i = 0; $i < $rows; $i++) {
     for ($j = 0; $j <= $i; $j++) {
         echo chr(65 + $j) . " ";
     }
-    echo "\n";
+    echo "<br>"; //html로 해서 해야하나?
+    
 }
-
 
 for ($i = $rows - 2; $i >= 0; $i--) {
     for ($j = 0; $j <= $i; $j++) {
         echo chr(65 + $j) . " ";
     }
-    echo "\n";
+    echo "<br>";
 }
 ?>
 
